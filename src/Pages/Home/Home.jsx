b38b8e64dc60c98906cbcdd6bea248d494215743
felip0za <135084133+felip0za@users.noreply.github.com>
2025-05-07 12:11:01 -0300

    commit

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -4,12 +4,11 @@ import Navbar from "../../Components/Navbar/Navbar";
 import "./Home.css";
 
 // Componente para o card
-const CardBox = ({ time, points, players, price, route, title }) => {
+const CardBox = ({ points, players, price, route, title }) => {
   const navigate = useNavigate();
 
   return (
     <div className="card-box" onClick={() => navigate(route)}>
-      <span className="card-time">Hoje - {time}</span>
       <div className="card-content">
         <p className="card-points">{points}pts</p>
         <h3 className="card-title">{title}</h3>
@@ -26,25 +25,24 @@ const CardBox = ({ time, points, players, price, route, title }) => {
   );
 };
 
-// Componente Home com a seção de campeonatos e lobbys grátis
+// Componente Home
 function Home() {
-  const navigate = useNavigate();
-
-  const cardData = [
-    { time: "Em 9min", points: 52, players: 4, price: 7, route: "/partida" },
-    { time: "Em 44min", points: 52, players: 0, price: 7, route: "/game/2" },
-    { time: "13h45", points: 52, players: 0, price: 7, route: "/game/3" },
-    { time: "13h45", points: 52, players: 0, price: 7, route: "/game/3" },
-    { time: "13h45", points: 52, players: 0, price: 7, route: "/game/3" },
-    { time: "13h45", points: 52, players: 0, price: 7, route: "/game/3" },
+  const FPLData = [
+    { title: "receba", points: 52, players: 4, price: 7, route: "/partida" },
+    { title: "receba", points: 52, players: 0, price: 7, route: "/game/2" },
+    { title: "receba", points: 52, players: 0, price: 7, route: "/game/3" },
+    { title: "receba", points: 52, players: 0, price: 7, route: "/game/3" },
+    { title: "receba", points: 52, players: 0, price: 7, route: "/game/3" },
+    { title: "receba", points: 52, players: 0, price: 7, route: "/game/3" },
   ];
 
-  const freeLobbyData = [
-    { time: "Em 5min", points: 20, players: 3, price: 0, route: "/free-lobby/1" },
-    { time: "Em 30min", points: 30, players: 0, price: 0, route: "/free-lobby/2" },
-    { time: "12h00", points: 50, players: 0, price: 0, route: "/free-lobby/3" },
-    { time: "13h00", points: 40, players: 0, price: 0, route: "/free-lobby/4" },
-    { time: "14h00", points: 60, players: 0, price: 0, route: "/free-lobby/5" },
+  const campeonatoData = [
+    { title: "receba", points: 20, players: 3, price: 0, route: "/free-lobby/1" },
+    { title: "receba", points: 30, players: 0, price: 0, route: "/free-lobby/2" },
+    { title: "receba", points: 50, players: 0, price: 0, route: "/free-lobby/3" },
+    { title: "receba", points: 40, players: 0, price: 0, route: "/free-lobby/4" },
+    { title: "receba", points: 60, players: 0, price: 0, route: "/free-lobby/5" },
+    { title: "receba", points: 60, players: 0, price: 0, route: "/free-lobby/5" },
   ];
 
   // Função para rolar até a seção de lobbys
@@ -52,26 +50,31 @@ function Home() {
     document.getElementById("free-lobby-section").scrollIntoView({ behavior: "smooth" });
   };
 
-  // Modificar a lógica para substituir um card
-  const modifiedLobbyData = freeLobbyData.length > 4
-    ? freeLobbyData.slice(0, 5).concat({
-        time: "Veja todos os lobbys",
-        points: 0,
-        players: 0,
-        price: 0,
-        route: "/profile",
+  // Modificar a lógica para substituir um card em campeonatos 
+  const modifiedCampeonatoData = campeonatoData.length > 4
+    ? campeonatoData.slice(0, 5).concat({
+        route: "/campeonatos",
         title: "ENTRAR NOS LOBBYS",
       })
-    : freeLobbyData;
+    : campeonatoData;
+
+    // Modificar a lógica para substituir um card em FPL
+    const modifiedFPLData = FPLData.length > 4
+    ? campeonatoData.slice(0, 5).concat({
+        route: "/FPL",
+        title: "ENTRAR NOS LOBBYS",
+      })
+    : FPLData;
 
   return (
     <>
       <Navbar />
       <div className="home-container">
+        {/* Seção de Disputas */}
         <h2 className="home-title">SELECIONE SUA DISPUTA</h2>
         <h2 className="type-title">FPL</h2>
         <div className="card-container">
-          {cardData.map((card, index) => (
+          {modifiedFPLData.map((card, index) => (
             <CardBox key={index} {...card} />
           ))}
         </div>
@@ -79,17 +82,10 @@ function Home() {
         {/* Seção de Campeonatos */}
         <h2 className="home-title">CAMPEONATOS</h2>
         <div className="card-container">
-          {modifiedLobbyData.map((card, index) => (
+          {modifiedCampeonatoData.map((card, index) => (
             <CardBox key={index} {...card} />
           ))}
         </div>
-
-        {/* Se o número de lobbys gratuitos for maior que 4, adicionar seta */}
-        {freeLobbyData.length > 4 && (
-          <div className="scroll-button" onClick={handleScrollToLobbys}>
-            <span>Veja mais →</span>
-          </div>
-        )}
       </div>
     </>
   );
